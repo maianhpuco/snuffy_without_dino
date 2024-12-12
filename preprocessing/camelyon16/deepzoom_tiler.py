336023695ac729df6514dbaea6d4ae4f52995810
Mai Anh Vu <maianhuel@gmail.com>
2024-12-12 12:18:49 -0600

    updata

--- preprocessing/camelyon16/deepzoom_tiler.py
+++ preprocessing/camelyon16/deepzoom_tiler.py
@@ -414,23 +414,23 @@ if __name__ == '__main__':
         print('Process slide {}/{} : {}'.format(idx + 1, len(all_slides), c_slide))
         start_time = time.time()
         
-        # tiler = DeepZoomStaticTiler(
-        #     slidepath=c_slide,
-        #     basename="WSI_temp",
-        #     mag_levels=levels,
-        #     base_mag=args.base_mag,
-        #     objective=args.objective,
-        #     format=args.format ,
-        #     tile_size=args.tile_size, #256,
-        #     overlap=args.overlap,
-        #     limit_bounds=True,
-        #     quality=args.quality, #75,
-        #     workers=args.workers, #8,
-        #     threshold=args.background_t,#0.5,
-        #     tile_label_csv=args.tile_label_csv,
-        #     slides_dir=args.slides_dir, 
-        # ).run()
-        # print(f"- Done tiling after {(time.time()-start_time)/60.0} mins")
+        tiler = DeepZoomStaticTiler(
+            slidepath=c_slide,
+            basename="WSI_temp",
+            mag_levels=levels,
+            base_mag=args.base_mag,
+            objective=args.objective,
+            format=args.format ,
+            tile_size=args.tile_size, #256,
+            overlap=args.overlap,
+            limit_bounds=True,
+            quality=args.quality, #75,
+            workers=args.workers, #8,
+            threshold=args.background_t,#0.5,
+            tile_label_csv=args.tile_label_csv,
+            slides_dir=args.slides_dir, 
+        ).run()
+        print(f"- Done tiling after {(time.time()-start_time)/60.0} mins")
         
         nested_patches(
             img_slide=c_slide, 
