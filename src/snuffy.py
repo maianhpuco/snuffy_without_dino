28085d46566121c31551fe1abc15ae4b96f9e877
Mai Anh Vu <maianhuel@gmail.com>
2024-12-12 20:43:10 -0600

    update

--- src/snuffy.py
+++ src/snuffy.py
@@ -7,7 +7,11 @@ import torch.optim as optim
 from lightly.utils.scheduler import CosineWarmupScheduler
 from torch.autograd import Variable
 from typing import Optional
-from custom_layers import MultiHeadedAttention, PositionwiseFeedForward, BClassifier, FCLayer, MILNet, VITFeatureExtractor, Encoder
+from custom_layers import (
+    MultiHeadedAttention, PositionwiseFeedForward, 
+    BClassifier, FCLayer, MILNet, 
+    VITFeatureExtractor, Encoder, EncoderLayer
+)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
